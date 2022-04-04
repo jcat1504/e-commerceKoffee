06db4cc0f08143ddfa6bc2c0ff6d769625c68b25
Jess Y <31145424+jcat1504@users.noreply.github.com>
2022-04-03 20:06:28 -0700

    Add files via upload

---
+++ src/pages/contact-form/contact-form.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ContactForm from '../../components/contact-form/contact-form';
+
+
+const ContactPage = () => (
+  <ContactForm />
+);
+
+export default ContactPage;
