b65870cc699a241390e6f5f1a6f2126a6f2fa87d
Jess Y <31145424+jcat1504@users.noreply.github.com>
2022-04-03 20:08:39 -0700

    Delete components directory

--- components/collections-overview/collections-overview.styles.jsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const CollectionsOverviewContainer = styled.div`
-  margin-top: 8%;
-  display: flex;
-  flex-direction: column;
-`;
--- components/directory/directory.styles.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const DirectoryMenuContainer = styled.div`
-  margin-top: 2%;
-  width: 100%;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-`;
